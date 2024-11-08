8324a0d88a557e2e1504e882353d9466e1041a91
ayanefreitass <ayanefreitas55@gmail.com>
2024-11-08 12:12:57 -0300

    bd.py

--- bd.py
+++ bd.py
@@ -1,6 +1,6 @@
 import mysql.connector
-from mysql.connector import errors
-from datetime import datetime
+import mysql.connector.errors
+
 class BD:
     _connection = None
 
@@ -14,14 +14,38 @@ class BD:
                 port='3306',
                 user='root',
                 password="",
-                database="contato"
+                database="telefone"
                     
                 )
                 return self._connection
-        except mysql.connection.errors.ProgrammingError as e:
-            print("Error:", e)
-            return{"Error:", e}
+        except mysql.connector.erros.ProgrammingError as error:
+            print("Error:", error)
+            return{"Error:", error}
         
     def close(self):
         self._connection.close()
         self._connection = None
+        
+    def buscarContatos(self):
+        self.criarConexão()
+        cursor = self._connection.cursor
+        
+        cursor.execute("select * from lista")
+        result = cursor.fetchall()
+        
+        contatos = []
+        
+        if len(result) > 0:
+            for contato in result:
+                contatos.append(
+                    {
+                    "nome": contato[0],
+                    "numero":contato[1],
+                    "cidade":[2]
+                    }
+                )
+
+    def adicionarcontato()
+bd = BD()
+
+print(bd.buscarContatos())
