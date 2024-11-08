cd4ccaf2ffa6654841c0f038850a5503462d96a2
ayanefreitass <ayanefreitas55@gmail.com>
2024-11-08 12:12:22 -0300

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
-from flask import Flask, render_template, session
-
+from flask import Flask, render_template,redirect, request, session
+from bd import BD
 
 app = Flask(__name__)
-app.secret_key = '123'
+bd = BD()
 
+app.secret_key = '123'
 user = {}
 
 @app.route('/')
 def index():
-    return render_template('login.html', e=False)
- 
-@app.route('/logar')
+    contatos = bd.buscaContatos()
+    return render_template('index.html', contatos=contatos)
+
+
+
+@app.roude("/deletar/str:<telefoneContato>")
+def deletar (telefoneContato):
+    bd.deletarcontato()
+    
+@app.roude("/novocontato", methods=["POST"])
+def novocontat():
+    nome = request.form["nome"]
+    telefone = request.form["telefone"]
+    cidade= request.form["cidade"]    
+    
+     
+@app.route('/lista')
 def logar():
-    session['origem'] = 'logar'
-    return render_template('logar.html')
+    session['origem'] = 'lista'
+    return render_template('lista.html')
     
 
 if __name__ == '__main__':
